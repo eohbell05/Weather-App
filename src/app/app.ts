6d68f17ec7507bf8256a899c94d57071dae19d06
unknown <G00466435@atu.ie>
2026-03-02 16:30:49 +0000

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './Services/data-service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('weather-app');
+export class App implements OnInit{
+  students:any[]=[];
+  weather:any[]=[];
+  constructor(private dataService:DataService){}
+  ngOnInit(): void {
+    this.dataService.GetStudentData().subscribe(
+      (data)=>{
+          this.students = data.students;
+          console.log(this.students);
+      }
+    );
+
+    this.dataService.GetWeatherData().subscribe(
+      (data)=>{
+          this.weather = data.weather;
+          console.log(this.weather);
+      }
+    )
+  }
 }
