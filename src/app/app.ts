529ae302730ede335b89b498a466a4e96a8724db
eohbell05 <G00466435@atu.ie>
2026-03-03 15:00:50 +0000

    Commented App.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,19 +9,25 @@ import { DataService } from './Services/data-service';
   styleUrl: './app.css'
 })
 export class App implements OnInit{
+  //Array to store students
   students:any[]=[];
+  //Arraya to store weather data
   weather:any[]=[];
+  //inject dataService into constructor
   constructor(private dataService:DataService){}
   ngOnInit(): void {
+    //Call service to get student data
     this.dataService.GetStudentData().subscribe(
       (data)=>{
+          //Store students from api response
           this.students = data.students;
           console.log(this.students);
       }
     );
-
+    //Call service to get weather data
     this.dataService.GetWeatherData().subscribe(
       (data)=>{
+          //Store weather info
           this.weather = data.weather;
           console.log(this.weather);
       }
